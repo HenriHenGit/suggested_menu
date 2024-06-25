a58ab47d3df544f2e896f21c9f1ae9790bc58946
HieuTrung <henri16102938@gmail.com>
2024-06-26 06:32:00 +0700

    Add data All Table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category_food;
+use App\Models\Category_ingre;
+use App\Models\Step_recipe;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +16,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(5)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+        $this->call([
+            CategoryFoodSeeder::class,
+            CategoryIngreSeeder::class,
+            NutriSeeder::class,
+            FoodSeeder::class,
+            FoodNutriSeeder::class,
+            IngredientSeeder::class,
+            IngredientDetailSeeder::class,
+            RecipeDetailSeeder::class,
+            StepRecipeSeeder::class,
+            OrderNutriSeeder::class,
         ]);
     }
 }
