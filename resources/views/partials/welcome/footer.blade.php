a224782e426ac08fce6c15d60fa92dfa35c03d8f
trungvo4 <0306211310@caothang.edu.vn>
2024-07-02 13:56:33 +0700

    Update footer.blade.php

--- resources/views/partials/welcome/footer.blade.php
+++ resources/views/partials/welcome/footer.blade.php
@@ -2,12 +2,14 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-lg-6 text-center pb-lg-4">
-                <h2 class="text-white mt-4 mb-3 fw-700">Beautiful designs to get you started</h2>
-                <p class="text-grey-500 me-lg-5 ms-lg-5">Create a really awesome website, choose the version
-                    that will suit your requirements in a best way.</p>
-                <div class="col-sm-12 text-center mt-4"><a href="#"
-                        class="bg-success btn btn-lg p-3 rounded-6 font-xsssss fw-700 ls-3 text-white w-175 text-uppercase">Purchase
-                        Now</a></div>
+                <h2 class="text-white mt-4 mb-3 fw-700">Thực Đơn Đa Dạng Dành Cho Mọi Nhóm Người</h2>
+                <p class="text-grey-500 me-lg-5 ms-lg-5">Tạo ra những bữa ăn hoàn hảo, lựa chọn thực đơn phù hợp nhất với
+                    nhu cầu của bạn.</p>
+                <div class="col-sm-12 text-center mt-4">
+                    <a href="{{ asset('login') }}"
+                        class="bg-success btn btn-lg p-3 rounded-6 font-xsssss fw-700 ls-3 text-white w-175 text-uppercase">Khám
+                        Phá Ngay</a>
+                </div>
             </div>
         </div>
     </div>
